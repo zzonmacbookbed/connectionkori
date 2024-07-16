3e9c0cd7996a9ce36dfcdace9e327cfa0d595472
zzonmacbookbed <113758657+zzonmacbookbed@users.noreply.github.com>
2024-07-16 21:07:24 +0900

    Initial Commit

---
+++ connectionkori/connectionkoriApp.swift
@@ -0,0 +1,17 @@
+//
+//  connectionkoriApp.swift
+//  connectionkori
+//
+//  Created by 제민국 on 7/16/24.
+//
+
+import SwiftUI
+
+@main
+struct connectionkoriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
